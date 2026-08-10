54fc5a3a518d0b6c78bf7ad8ea30f6e7045819fb
leshan-rkt <leshanjoseph04@gmail.com>
2026-08-10 19:23:06 +0300

    sqlite

--- GariCare/settings.py
+++ GariCare/settings.py
@@ -90,19 +90,30 @@ WSGI_APPLICATION = 'GariCare.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'raketaleshan_garicare',
-        'USER': 'raketaleshan',
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': 'mysql-raketaleshan.alwaysdata.net',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+# ✅ DATABASE CONFIGURATION
+import os
+
+if os.getenv('DB_ENV') == 'production' or config('DEBUG', default=True) == 'False':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'raketaleshan_garicare',
+            'USER': 'raketaleshan',
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': 'mysql-raketaleshan.alwaysdata.net',
+            'PORT': '3306',
+            'OPTIONS': {
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            },
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
