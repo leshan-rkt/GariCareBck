a6faf3dd02d475b895367c1ce4ef6350ef6d5136
leshan-rkt <leshanjoseph04@gmail.com>
2026-08-17 12:30:02 +0300

    path

--- GariCare/urls.py
+++ GariCare/urls.py
@@ -18,8 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/users/', include('users.urls')),
-    path('api/cars/', include('cars.urls')),
-    path('api/maintenance/', include('maintenance.urls')),
+    path('garicare/admin/', admin.site.urls),
+    path('garicare/api/users/', include('users.urls')),
+    path('garicare/api/cars/', include('cars.urls')),
+    path('garicare/api/maintenance/', include('maintenance.urls')),
 ]
