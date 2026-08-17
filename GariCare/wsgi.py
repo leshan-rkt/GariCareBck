bd13833c034766551c453918bb683cd6fb5808fa
leshan-rkt <leshanjoseph04@gmail.com>
2026-08-17 11:57:04 +0300

    wsgi

--- GariCare/wsgi.py
+++ GariCare/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
 
 from django.core.wsgi import get_wsgi_application
-
+sys.path.append("/home/raketaleshan/www/GariCareBck")
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GariCare.settings')
 
 application = get_wsgi_application()
