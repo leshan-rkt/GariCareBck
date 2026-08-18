6448a6baef3570cc08621958c325d6f8e82b2a07
leshan-rkt <leshanjoseph04@gmail.com>
2026-08-18 12:34:53 +0300

    origin

--- GariCare/settings.py
+++ GariCare/settings.py
@@ -31,7 +31,23 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.alwaysdata.net', 'www.raketaleshan.alwaysdata.net','localhost:5173/',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://maziwa-sync-react-class.vercel.app"
+]
 
+
+# CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     "https://raketaleshan.alwaysdata.net",
+#     "http://raketaleshan.alwaysdata.net",
+#     "http://localhost:5173",
+#     "http://localhost:5174",
+#     "http://127.0.0.1:5173",
+#     "http://127.0.0.1:5174",
+# ]
+# CORS_ALLOW_CREDENTIALS = True
 # Application definition
 
 INSTALLED_APPS = [
@@ -172,16 +188,6 @@ SIMPLE_JWT = {
 }
 
 # CORS Settings
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
-    "https://raketaleshan.alwaysdata.net",
-    "http://raketaleshan.alwaysdata.net",
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "http://127.0.0.1:5173",
-    "http://127.0.0.1:5174",
-]
-CORS_ALLOW_CREDENTIALS = True
 
 # M-Pesa
 MPESA_ENVIRONMENT = config('MPESA_ENVIRONMENT')
